8d3ffecc9bbc7099653000c0489ce319bc7b1912
princerajkr45 <priyance5204@gmail.com>
2024-09-03 23:38:32 +0530

    adjust projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -23,7 +23,7 @@ function Projects() {
                 </p>
             </section>
 
-            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-auto px-4 max-w-7xl">
+            <section className="grid grid-cols-1 text-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mx-auto px-4 max-w-7xl">
                 {React.Children.toArray(
                     projectDetails.map(
                         ({ title, image, description, techstack, previewLink, githubLink }) => (
