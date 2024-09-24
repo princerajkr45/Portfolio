4618c350a970de50c03adad060f33531f18d6e63
princerajkr45 <priyance5204@gmail.com>
2024-09-24 09:11:27 +0530

    technogies adjustment

--- src/Pages/Technologies.jsx
+++ src/Pages/Technologies.jsx
@@ -39,9 +39,9 @@ function Technologies() {
                         Technologies I've been working with recently
                     </p>
                 </section>
-                <section className="grid grid-cols-3 justify-center sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-8 md:gap-10 pt-6 px-24">
+                <section className="grid grid-cols-3 justify-center sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-8 md:gap-10 pt-6 px-8">
                     <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-                        <img src={html} title="HTML" alt="HTML" className='w-16 h-16 md:w-20 md:h-20' />
+                        <img src={html} title="HTML" alt="HTML" className=' w-16 h-16 md:w-20 md:h-20' />
                     </a>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
                         <img src={css} title="CSS" alt="CSS" className='w-16 h-16 md:w-20 md:h-20' />
@@ -73,7 +73,7 @@ function Technologies() {
                         Tools
                     </h1>
                 </section>
-                <section className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6 md:gap-10 pt-6 px-24">
+                <section className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-6 md:gap-10 pt-6 px-8">
                     <a href="https://code.visualstudio.com/">
                         <img src={vscode} title="Visual Studio Code" alt="Visual Studio Code" className='w-20 h-20 md:w-24 md:h-24' />
                     </a>
